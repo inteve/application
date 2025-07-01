91a3497ebae0edd7d2451f536444b84a09921326
Jan Pecha <janpecha@email.cz>
2025-07-01 11:38:44 +0200

    Added declare(strict_types=1)

--- src/DI/PresenterMapping.php
+++ src/DI/PresenterMapping.php
@@ -1,5 +1,7 @@
 <?php
 
+	declare(strict_types=1);
+
 	namespace Inteve\Application\DI;
 
 	use Nette\Application\IPresenterFactory;
--- src/TSecured.php
+++ src/TSecured.php
@@ -1,5 +1,7 @@
 <?php
 
+	declare(strict_types=1);
+
 	namespace Inteve\Application;
 
 	use Nette\Application\ForbiddenRequestException;
